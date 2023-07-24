4bb266219a6bf2a1023416f82988b6d90f65256b
Ruturaj Sant <sant.ruturaj2001@gmail.com>
2023-07-24 22:17:11 +0530

    header

--- src/app/rooms/rooms-list/rooms-list.component.ts
+++ src/app/rooms/rooms-list/rooms-list.component.ts
@@ -1,12 +1,32 @@
 import { RoomsList } from './../room';
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  OnInit,
+  OnChanges,
+  EventEmitter,
+  SimpleChanges,
+} from '@angular/core';
+import { ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-rooms-list',
   templateUrl: './rooms-list.component.html',
   styleUrls: ['./rooms-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoomsListComponent implements OnInit {
+export class RoomsListComponent implements OnInit, OnChanges {
   @Input() rooms: RoomsList[] = [];
+  @Input() title: String = '';
+  @Output() selectedRoom = new EventEmitter<RoomsList>();
   ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['title']) {
+      this.title = changes['title'].currentValue.toUpperCase();
+    }
+  }
+  selectRoom(room: RoomsList) {
+    this.selectedRoom.emit(room);
+  }
 }
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, ViewChild } from '@angular/core';
+import { HeaderComponent } from '../header/header.component';
 import { Room, RoomsList } from './room';
 
 @Component({
@@ -6,21 +7,44 @@ import { Room, RoomsList } from './room';
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.css'],
 })
-export class RoomsComponent {
+export class RoomsComponent implements DoCheck {
+  ngDoCheck(): void {
+    throw new Error('Method not implemented.');
+  }
   hotelName = 'Hotel One';
   numberofRooms = 230;
   hideRooms = false;
+  selectedRoom!: RoomsList;
   // availableRooms: 10,
   //   bookedRooms: 5,
   //   totalRooms: 20,
   toggle() {
     this.hideRooms = !this.hideRooms;
   }
+  selectRoom(room: RoomsList) {
+    this.selectedRoom = room;
+  }
+  addRoom() {
+    const room: RoomsList = {
+      roomType: 'Deluxe',
+      amenities: 'Wifi',
+      price: 100,
+      photos: 'https://picsum.photos/200',
+      checkInTime: new Date('11-Nov-2023'),
+      checkOutTime: new Date('15-Nov-2023'),
+      roomNumber: 4,
+    };
+    this.roomList = [...this.roomList, room];
+    console.log(this.roomList);
+  }
+  title: String = 'Rooms List';
   room: Room = {
-    availableRooms: 10,
+    availableRooms: 3,
     bookedRooms: 5,
-    totalRooms: 20,
+    totalRooms: 230,
   };
+  @ViewChild(HeaderComponent) headerComponent!: HeaderComponent;
+
   roomList: RoomsList[] = [
     {
       roomType: 'Deluxe',
